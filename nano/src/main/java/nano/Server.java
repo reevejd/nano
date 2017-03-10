2391c3f1fabbaf4a24d66501a9c05060d971cb05
James Reeve <reevejamesd@gmail.com>
2017-03-10 13:24:14 -0500

    add comments explaining the use of command line arguments

--- nano/src/main/java/nano/Server.java
+++ nano/src/main/java/nano/Server.java
@@ -15,9 +15,16 @@ import java.util.List;
 public class Server {
 
     public static void main(String[] args) throws IOException {
+        
         long start = System.currentTimeMillis();
-        String rootFolder = args.length >= 1 ? args[0] : ".";
+
+        /* A string parameter can be passed to the main function to specify a root folder,
+        otherwise by default the local directory is the root */
+        String rootFolder = args.length >= 1 ? args[0] : "."; 
+
+        /* A port can be specified as the 2nd argument to main, otherwise 4242 is the default port */
         int port = args.length >= 2 ? Integer.parseInt(args[1]) : 4242;
+        
         Path root = Paths.get(rootFolder);
         HttpServer server = HttpServer.create(new InetSocketAddress(port), 0);
         List<Path> discovered = Contexts.discoverContexts(root);
