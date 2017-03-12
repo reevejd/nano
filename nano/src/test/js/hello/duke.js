f8245a2815c7ea363983b8e7dffdb30b1ee0a589
James Reeve <reevejamesd@gmail.com>
2017-03-12 00:05:49 -0500

    comment duke.js

--- nano/src/test/js/hello/duke.js
+++ nano/src/test/js/hello/duke.js
@@ -1,4 +1,7 @@
 function process(method, requestHeaders, responseHeaders, request, response) {
+    /*
+     * A very simple http handler. Accepts and prints the request, writes a response, returns status code 200.
+     */
     print(requestHeaders.values());
     responseHeaders.add("hey", "ho");
 
