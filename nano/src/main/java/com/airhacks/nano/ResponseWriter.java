8892efb9b8484e42d421be5e660c2058df727fa1
James Reeve <reevejamesd@gmail.com>
2017-03-12 00:07:55 -0500

    comment ResponseWriter.java

--- nano/src/main/java/com/airhacks/nano/ResponseWriter.java
+++ nano/src/main/java/com/airhacks/nano/ResponseWriter.java
@@ -5,6 +5,9 @@ package com.airhacks.nano;
  * @author airhacks.com
  */
 public interface ResponseWriter {
-
+    /*
+     * A very simple interface used when instantiating a StringBuilder 
+     * that is passed to the HTTP handler so a response can be written.
+     */
     void write(String content);
 }
